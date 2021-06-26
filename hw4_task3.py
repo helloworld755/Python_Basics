41a6ead562104cfa4e173592fb718c003c1c8b2c
helloworld <ppyosel@gmail.com>
2021-06-26 13:46:28 +0300

    homework is done (5 tasks)

---
+++ hw4_task3.py
@@ -0,0 +1,29 @@
+import requests as rq
+from decimal import Decimal
+
+
+def currency_rates(code):
+    response = rq.get('http://www.cbr.ru/scripts/XML_daily.asp')
+    encodings = rq.utils.get_encoding_from_headers(response.headers)
+    content = response.content.decode(encoding=encodings)
+    start = content.find(code.upper())
+    if start != -1:
+        val_start = content.find("<Value>", start, len(content)) + 7
+        val_end = content.find("</Value>", start, len(content))
+        value = content[val_start:val_end]
+        print(f'{code.upper()}: {str(Decimal(value.replace(",", ".")))}')
+        # date from header
+        print(f'Date from Header: {response.headers["Date"]}')
+        # date from Body
+        val_start = content.find('ValCurs Date="') + 14
+        val_end = val_start + 10
+        value = content[val_start:val_end]
+        print(f'Date from Body: {str(value)}')
+    else:
+        print(f'{code.upper()}: None')
+
+
+if __name__ == '__main__':
+    currency_rates("UsD")
+    currency_rates("euR")
+    currency_rates("ABc")
